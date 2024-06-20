87558ded8cf722d0f0a975f89f489a1d9409d2d4
Ritesh <riteshhit2017@gmail.com>
2024-06-21 00:11:12 +0530

    first commit

---
+++ untitled/src/main/java/org/example/NumberPrinter/Main.java
@@ -0,0 +1,24 @@
+package org.example.NumberPrinter;
+
+public class Main {
+    public static void main(String[] args) {
+        //with normal
+        for(int i = 1; i <= 10000; i++){
+            if(i == 40) {
+                System.out.println("Ritesh");
+            }
+            Thread t = new Thread(new NumberPrinter(i));
+            t.start();
+        }
+
+        //with lambda expression no need to create numberprinter class
+//        for(int i = 1; i <= 10000; i++){
+//            if(i == 40) {
+//                System.out.println("Ritesh");
+//            }
+//            int number = i;
+//            Thread t = new Thread(() -> System.out.println(number+" "+Thread.currentThread().getName()));
+//            t.start();
+//        }
+    }
+}
---
+++ untitled/src/main/java/org/example/NumberPrinter/NumberPrinter.java
@@ -0,0 +1,12 @@
+package org.example.NumberPrinter;
+
+public class NumberPrinter implements Runnable{
+    int numberToPrint;
+    public NumberPrinter(int numberToPrint){
+        this.numberToPrint = numberToPrint;
+    }
+    @Override
+    public void run() {
+        System.out.println("Printing " + numberToPrint + " from "+ Thread.currentThread().getName());
+    }
+}
